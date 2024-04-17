dfc4354ca1b46e70903f919bf7fc25262df80111
Abel Zverev <abelatoxz@gmail.com>
2024-04-17 14:25:13 +0000

    funciona

--- cli-interactivon't-yml/main.php
+++ cli-interactivon't-yml/main.php
@@ -2,6 +2,8 @@
 use Symfony\Component\Yaml\Yaml;
 require __DIR__ . '/vendor/autoload.php';
 $value = Yaml::parseFile('./conf.yaml');
-print_r ($value);
+foreach ($value as $key => $value) {
+  echo $value;
+}
 ?>
 
